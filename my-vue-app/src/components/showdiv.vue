1ed5a7e394b1aa7fa06364b827a429e7b74db274
nsakwa <nsakwa@gmail.com>
2025-03-31 14:55:57 +0300

    vue js show

---
+++ my-vue-app/src/components/showdiv.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <div v-show="showDiv">Div tag with v-show</div>
+        <div v-if="showDiv">Dive tag with v-if</div>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            showDiv:true,
+        };
+    };
+};
+</script>
+<style scoped>
+#app {
+    border:black dashed 1px;
+    width: 80%;
+    padding: 0 20px 20px 20px;
+}
+#app div {
+    padding :20px;
+    margin: 10px;
+    display: inline-block;
+    font-weight: bold;
+    border:solid black 1px;
+}
+</style>
